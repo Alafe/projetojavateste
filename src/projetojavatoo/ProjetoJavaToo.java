62deae2391e572a06a834a984d72e583e5e9f253
Alafe <alaferodrigo18@gmail.com>
2018-08-30 15:12:07 -0300

    add uma mensagem

--- src/projetojavatoo/ProjetoJavaToo.java
+++ src/projetojavatoo/ProjetoJavaToo.java
@@ -15,7 +15,7 @@ public class ProjetoJavaToo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Teste");
     }
     
 }
